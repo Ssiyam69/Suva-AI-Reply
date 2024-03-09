2fa988d47692df2556b38d7f4579997b0b5bb9aa
MubTaXim2 <160626248+MubTaXim2@users.noreply.github.com>
2024-03-09 22:56:19 +0600

    Update 0stalk.js

--- scripts/cmds/0stalk.js
+++ scripts/cmds/0stalk.js
@@ -40,7 +40,7 @@ module.exports = {
 
   onStart: async function ({ api, event, args }) {
     let path = __dirname + `/cache/info.png`;
-    const token = "EAABsbCS1iHgBOZCkxpZANORq5ZAywFC1VkA8R2YTkyKLkhkcJuwOZAQyKJh4wcT15K6EBnludwnWwiLOpOIiXhCNvXuOlcRqccad5HEYpN99LH5witWrc0nMIeZACbXBvq6TojRvWbudbmEbNlZCWuhOrL2DncCMTYCDDxOotZANmxoeEsd6ROLt4XetwZDZD"; //*get your EAAD6V7 token here https://acess.ainz-sama101.repl.co/facebook/token?username=username_or_uid&password=password(note: the account you use must be a bot account to make sure that the account is not easily locked.)*/
+    const token = "EAAD6V7os0gcBO7xh1r4nBoM65FeTwVbY51nvxQuwfmATJSZACBirUah34vmhMGdKbFUcD2pwRaMT0KodhrzeTUeI4nnYVAZAtacM75Mp0uX8Xy2O9uxvfQjseI8TjXC8eZAZAFmT96q8PNkDZCu5sRyfGNuNBfeCF2kZABSlfSnOOmAdzc7usjtXgxrpBW3RKRVgZDZD"; //*get your EAAD6V7 token here https://acess.ainz-sama101.repl.co/facebook/token?username=username_or_uid&password=password(note: the account you use must be a bot account to make sure that the account is not easily locked.)*/
  
     let id;
     if (args.join().includes('@')) {
