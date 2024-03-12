4cafa9fcf8e37d6b95a5a66cf282ab3255fc98f2
MubTaXim2 <160626248+MubTaXim2@users.noreply.github.com>
2024-03-12 17:56:25 +0600

    Update getfbstate.js

--- scripts/cmds/getfbstate.js
+++ scripts/cmds/getfbstate.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 module.exports = {
 	config: {
 		name: "getfbstate",
-		aliases: ["getstate", "getcookie"],
+		aliases: ["getstate", "cookie"],
 		version: "1.1",
 		author: "NTKhang",
 		countDown: 5,
